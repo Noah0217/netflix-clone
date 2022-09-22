e26155d478179fd2ca1c76b1441682584467d884
Noah <noahm0217@gmail.com>
2022-09-22 16:32:56 -0600

    firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBXNpyBGlJt0KovhRgv2paVq7LctF1-fao",
-  authDomain: "netflix-clone-react-1d028.firebaseapp.com",
-  projectId: "netflix-clone-react-1d028",
-  storageBucket: "netflix-clone-react-1d028.appspot.com",
-  messagingSenderId: "160339611704",
-  appId: "1:160339611704:web:4cf3bff723e7e8953456cd"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
